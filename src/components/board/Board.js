9cfc5c5f31468cf6ea6ddc54490c2fad8284b944
Matt <lockdown1926@gmail.com>
2022-03-01 10:43:53 -0700

    Update Board.js

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import classes from './Board.module.css'
 import Square from './square/Square';
 
@@ -8,9 +9,12 @@ function Board(props) {
 
     let xSquare = 0;
 
-    const squares = [];
+    const [squares, setSquares] = useState([]);
     console.log(fen);
 
+    // need 2 functions, and an or to pass it depending on if the first condition is true. 
+    //The first should select the piece, and highlight valid moves
+    //the second should let the player make a valid move, and return a prompt to do so if no valid move is made.
     const selectPiece = (pieceName, position, index) => {
         const color = pieceName.split(' ');
         if(pieceName) {
@@ -18,31 +22,31 @@ function Board(props) {
         }
         console.log(position);
         console.log(index);
+
         if (color[0] === "White")
         {
-            console.log(squares[index - 8].props.position)
+            console.log(squares[index - 8].position)
         }
         else if (color[0] === "Black")
         {
-            console.log(squares[index + 8].props.position)
+            console.log(squares[index + 8].position)
         }
+        
+        console.log(squares[0]);
       }
 
+      // need to map the pieces through the FEN so that they're able to be updated
     for (let y = 0; y < fen.length; y++) {
         for (let x = 0; x < fen[y].length; x++) {
-            
 
             if (Number(fen[y][x])) {
                 for (let i = 0; i < fen[y][x]; i++) {
                     let number = xSquare + y;
-                    squares.push(<Square
-                        selectPiece={selectPiece}
-                        index={squares.length}
-                        piece={''}
-                        number={number}
-                        key={xAxis[xSquare] + yAxis[y]}
-                        position={xAxis[xSquare] + yAxis[y]}
-                    />)
+                    squares.push({
+                        piece: '',
+                        number: number,
+                        position: xAxis[xSquare] + yAxis[y],
+                })
                     if (xSquare != 7) {
                     xSquare++;
                     }
@@ -53,14 +57,11 @@ function Board(props) {
             }
             else {
                 let number = xSquare + y;
-                squares.push(<Square
-                    selectPiece={selectPiece}
-                    index={squares.length}
-                    piece={fen[y][x]}
-                    number={number}
-                    key={xAxis[xSquare] + yAxis[y]}
-                    position={xAxis[xSquare] + yAxis[y]}
-                />)
+                squares.push({
+                        piece: fen[y][x],
+                        number: number,
+                        position: xAxis[xSquare] + yAxis[y],
+                })
                 if (xSquare != 7) {
                     xSquare++;
                     }
@@ -71,11 +72,19 @@ function Board(props) {
             }
         }
 
-    
+        
 
+    
 
     return <div
     className={classes.Board}
-    >{squares}</div>;
+    >{squares.map((square, index) => <Square
+    selectPiece={selectPiece}
+    index={index}
+    piece={square.piece}
+    number={square.number}
+    key={square.position}
+    position={square.position}
+    />)}</div>;
 }
 export default Board;
