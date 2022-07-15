b0541696b070b2ec3f006551f463fff7f86c4a96
Matt <lockdown1926@gmail.com>
2022-07-14 21:18:58 -0600

    Update Chess.js

--- src/components/chess/Chess.js
+++ src/components/chess/Chess.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Board from '../board/Board';
 import classes from './Chess.module.css';
 
 function Chess() {
   const [fen, setFen] = useState(['rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR', 'w', 'KQkq', '-']); // initialize the FEN, and put it an an array, 0 is the positions, 1 is who's turn
   // 2 is who can castle, 3 is if there is an en passent available
-
-  const [fenPosition, setFenPosition] = useState(fen[0]); // Take just the part that positions the pieces
-  const [fenPiecePosition, setFenPiecePosition] = useState(fenPosition.split('/')); // break it down into an array, row by row for when its set.
+  
+  const fenPosition = fen[0]; // Take just the part that positions the pieces
+  const fenPiecePosition = fenPosition.split('/'); // break it down into an array, row by row for when its set.
   const xAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']; //Set the rows and files for assigning each squares position
   const yAxis = [8, 7, 6, 5, 4, 3, 2, 1];
   let xSquare = 0;
@@ -16,15 +16,36 @@ function Chess() {
   const [selectedPiece, setSelectedPiece] = useState(-1); // I'm going to pass two functions down to onClick. The first will be if selectedPiece is -1, and it will set the 
   // index of the piece that is selected. Highlighting that piece, and any valid moves that piece can make.
   const [potentialMoves, setPotentialMoves] = useState([]); //going to fill this with potential moves that can be used in the second part of selectPiece
-  for (let y = 0; y < fenPiecePosition.length; y++) { //this sets the initial FEN, and loads it all into a dummy array, before setting the state, so it doesn't load endlessly
-    for (let x = 0; x < fenPiecePosition[y].length; x++) {
 
-      if (Number(fenPiecePosition[y][x])) {
-        for (let i = 0; i < fenPiecePosition[y][x]; i++) {
+    for (let y = 0; y < fenPiecePosition.length; y++) { //this sets the initial FEN, and loads it all into a dummy array, before setting the state, so it doesn't load endlessly
+      for (let x = 0; x < fenPiecePosition[y].length; x++) {
+
+        if (Number(fenPiecePosition[y][x])) {
+          for (let i = 0; i < fenPiecePosition[y][x]; i++) {
+            let number = xSquare + y;
+            startingSquares.push({
+              piece: {
+                pieceType: '',
+                hasMoved: false,
+                isCheck: false,
+              },
+              number: number,
+              position: xAxis[xSquare] + yAxis[y],
+              highlighted: false,
+            })
+            if (xSquare != 7) {
+              xSquare++;
+            }
+            else {
+              xSquare = 0;
+            }
+          }
+        }
+        else {
           let number = xSquare + y;
           startingSquares.push({
             piece: {
-              pieceType: '',
+              pieceType: fenPiecePosition[y][x],
               hasMoved: false,
               isCheck: false,
             },
@@ -40,28 +61,10 @@ function Chess() {
           }
         }
       }
-      else {
-        let number = xSquare + y;
-        startingSquares.push({
-          piece: {
-            pieceType: fenPiecePosition[y][x],
-            hasMoved: false,
-            isCheck: false,
-          },
-          number: number,
-          position: xAxis[xSquare] + yAxis[y],
-          highlighted: false,
-        })
-        if (xSquare != 7) {
-          xSquare++;
-        }
-        else {
-          xSquare = 0;
-        }
-      }
     }
-  }
+  
 
+  
   const [squares, setSquares] = useState([...startingSquares]);
   // need 2 paths in the function, depending on if a piece is selected. 
   //The first should select the piece, and highlight valid moves
@@ -91,7 +94,7 @@ function Chess() {
         newSquares[index].highlighted = !newSquares[index].highlighted;
         setSquares(newSquares);
 
-        
+
       }
       else {
         alert("Please select a valid piece");
@@ -99,7 +102,7 @@ function Chess() {
     }
 
     else {
-      
+
     }
 
 
@@ -116,14 +119,33 @@ function Chess() {
 
   }
 
+  const updateFEN = (e) => {
+    setChangedFEN(e.target.value);
+  }
+
+  const submitFEN = () => {
+    var newFenArray = fen;
+    newFenArray[0] = changedFEN;
+    setFen(newFenArray);
+    setSquares([...startingSquares]);
+    console.log("test");
+  }
+
+  const [changedFEN, setChangedFEN] = useState();
 
   return <div className={classes.Chess}>
+    <div className={classes.FEN_display}>
+      <span className={classes.Input_Line}><p>Input FEN String:</p><input type="text" value={changedFEN} onChange={updateFEN}></input></span><button onClick={submitFEN}>Update</button>
+      <p>Current FEN is: {fen[0]}</p>
+    </div>
     <Board
+      key={fen}
       squares={squares}
       selectPiece={selectPiece}
     />
   </div>;
 }
+
 
 export default Chess;
 
